98d8c269160d165b2a15746e850663b65caabf1f
Logan Koshenka <koshenka27@gmail.com>
2021-05-19 23:03:01 -0400

    Initial Commit

---
+++ stacksBabyyyyyyyy/stacksBabyyyyyyyyApp.swift
@@ -0,0 +1,17 @@
+//
+//  stacksBabyyyyyyyyApp.swift
+//  stacksBabyyyyyyyy
+//
+//  Created by Logan Koshenka on 5/19/21.
+//
+
+import SwiftUI
+
+@main
+struct stacksBabyyyyyyyyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
