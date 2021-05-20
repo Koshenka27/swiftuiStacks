bfeaee4de1180477510b00930de5689ae4714514
Logan Koshenka <koshenka27@gmail.com>
2021-05-20 09:21:47 -0400

    done

--- stacksBabyyyyyyyy/ContentView.swift
+++ stacksBabyyyyyyyy/ContentView.swift
@@ -9,8 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.7353854029, green: 0.8395714863, blue: 1, alpha: 1)), Color(#colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1))]), startPoint: .top, endPoint: .bottom)
+            
+            VStack(alignment: .center) {
+                ZStack {
+                    Circle()
+                        .frame(width: 100, height: 100)
+                        .foregroundColor(Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)))
+                    Image(systemName: "tortoise")
+                        .font(.system(size: 50, weight: .bold))
+                }
+                
+                HStack(alignment: .center) {
+                    VStack(alignment: .leading, spacing: 5) {
+                        Text("Hi, I'm the title")
+                            .font(.system(size: 20, weight: .bold))
+                        
+                        Text("subtitle".uppercased())
+                            .font(.system(size: 14, weight: .light))
+                    }
+                    
+                    Spacer()
+                    
+                    VStack {
+                        Image(systemName: "figure.walk")
+                            .font(.system(size: 30, weight: .bold))
+                        
+                        Text("Frank")
+                            .font(.caption)
+                    }
+                }
+                .padding()
+            }
+        }
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
